b0cfc2daa97bd1e51df1ef7bdb7925685ef68c35
apurbochaki-lab <apurbokumarchaki@gmail.com>
2026-04-26 23:01:01 +0600

    Added dynamic news details page by news_id

--- src/app/(main)/news-details/[details_Id]/page.jsx
+++ src/app/(main)/news-details/[details_Id]/page.jsx
@@ -1,10 +1,54 @@
+import RightSidebar from '@/components/homepage/news-section/RightSidebar';
+import { getNewsDetailsById } from '@/lib/data';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
 
-const NewsDetailsPage = () => {
+const NewsDetailsPage = async ({ params }) => {
+    const { details_Id } = await params;
+    // console.log(details_Id)
+
+    const newsDetails = await getNewsDetailsById(details_Id);
+    // const {_id, image_url, title, details, category_id} = newsDetails;
+    // {newsDetails.map(d => console.log(d))}
+    console.log(newsDetails)
+
     return (
-        <div>
-            News Details Page
-        </div>
+        <section className='container mx-auto'>
+            <h2 className='text-lg font-bold mb-5'>Dragon News</h2>
+
+            <div className='grid grid-cols-12 gap-5'>
+                {/* left Side */}
+                <div className='col-span-9'>
+                    {
+                        newsDetails.map(detail => {
+                            return <div key={detail._id}>
+                                <div className="card bg-base-100 border border-gray-300 shadow-sm">
+                                    <figure className='m-5'>
+                                        <Image src={detail.image_url} width={1200} height={500} alt={detail.title}></Image>
+                                    </figure>
+                                    <div className="card-body space-y-3">
+                                        <h2 className="card-title text-3xl">{detail.title}</h2>
+                                        <p className='text-lg text-gray-500'>{detail.details}</p>
+                                        <div className="card-actions justify-start">
+                                            <Link href={`/category/${detail.category_id}`}>
+                                                <button className="btn bg-[#D72050] text-white p-6 font-bold"><FaArrowLeft /> All news in this category</button>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
+
+                {/* Right Side */}
+                <div className=' col-span-3'>
+                    <RightSidebar></RightSidebar>
+                </div>
+            </div>
+        </section>
     );
 };
 
