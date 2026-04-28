aee6dd84d8c0f12f9946ca0dd8f5ecbf0a65ecb8
apurbochaki-lab <apurbokumarchaki@gmail.com>
2026-04-28 16:26:39 +0600

    Added dns in proxey.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,9 @@
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
-
 
 export async function proxy(request) {
 
